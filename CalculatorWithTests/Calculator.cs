8036e1d09b7e63ea1360bd0a7fe03fd7408c4f42
YShin <yookyung.shin@gmail.com>
2019-04-04 19:38:59 -0500

    Calculator class with 5 methods added

---
+++ CalculatorWithTests/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorWithTests
+{
+    class Calculator
+    {
+        public int TestAdd(num1, num2)
+        {
+            return 0;
+        }
+        public int TestSubtract(num1, num2)
+        {
+            return 0;
+        }
+        public int TestMultiply(num1, num2)
+        {
+            return 0;
+        }
+        public int TestDivide(num1, num2)
+        {
+            return 0;
+        }
+        public int TestFactorial(num1, num2)
+        {
+            return 0;
+        }
+    }
+}

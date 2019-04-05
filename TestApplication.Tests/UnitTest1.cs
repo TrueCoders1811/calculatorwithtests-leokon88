30359216223f41abc3bfa35e2e49c9cdfdea837d
YShin <yookyung.shin@gmail.com>
2019-04-04 20:20:02 -0500

    Test for Divide added

--- TestApplication.Tests/UnitTest1.cs
+++ TestApplication.Tests/UnitTest1.cs
@@ -21,5 +21,48 @@ namespace TestApplication.Tests
             Assert.Equal(expectedValue, actualResult);
             //Assert
         }
+
+        [Theory]
+        [InlineData(8,3,5)]
+        [InlineData(-1, -1, 0)]
+        [InlineData(4, -3, 7)]
+        public void TestForSubtractMethod(int num1, int num2, int expectedValue)
+        { 
+            Calculator calc = new Calculator();
+            //Arrange
+            int actualResult = calc.TestSubtract(num1, num2);
+            //Act
+            Assert.Equal(expectedValue, actualResult);
+            //Assert
+         }
+
+        [Theory]
+        [InlineData(4,5,20)]
+        [InlineData(-4, 1, -4)]
+        [InlineData(5, 2, 2.5)]
+        public void TestMultiply(double num1, double num2, double expectedValue)
+        {
+            Calculator cal = new Calculator();
+            //Arrange
+            double actualResult = cal.TestMultiply(num1,num2);
+            //Act
+            Assert.Equal(expectedValue, actualResult);
+            //Assert
+        }
+
+        [Theory]
+        [InlineData(1.0,4,0.25)]
+        [InlineData(-13.0,4,3.25)]
+        [InlineData(0, 0, 0)]
+        public void TestDivide(double num1, double num2, double expectedValue)
+        {
+            Calculator calc = new Calculator();
+            //Arrange
+            double actualResult = calc.TestDivide(num1, num2);
+            //Act
+            Assert.Equal(expectedValue, actualResult);
+            //Assert
+
+        }
     }
 }
